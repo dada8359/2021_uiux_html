3c1260cabd8c615317550438173f6c8f43694db3
다다미무침 <87959460+dada8359@users.noreply.github.com>
2021-09-13 18:21:33 +0900

    up

--- 04company/js/script2.js
+++ 04company/js/script2.js
@@ -1,6 +1,27 @@
 $(document).ready(function(){
-    $(".slider").bxSlider({
+    const slider1 = $(".slider").bxSlider({
         controls:false
+    });
+
+
+    $(".toggle").click(function(){
+        const toggle = $(".toggle").attr("class"); // value toggle
+        console.log(toggle);
+
+        if( toggle == "toggle"){
+            //$(".toggle").addClass("active");
+            document.getElementsByClassName("toggle")[0].classList.add("active");
+            $(".gnb").addClass("view");
+        }else{
+            //$(".toggle").removeClass("active");
+            document.getElementsByClassName("toggle")[0].classList.remove("active");
+            $(".gnb").removeClass("view");
+        }
+    });
+      
+    $(window).resize(function(){
+        $(".toggle").removeClass("active");
+        $(".gnb").removeClass("view");
     });
 
 
@@ -13,7 +34,4 @@ $(document).ready(function(){
 
 
 
-
-
-    
 });
