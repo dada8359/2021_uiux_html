c0e8197fad307d8dd738facdcb02894ec69cf7e6
다다미무침 <87959460+dada8359@users.noreply.github.com>
2021-09-14 18:16:26 +0900

    up

--- 04company/js/myscript.js
+++ 04company/js/myscript.js
@@ -2,23 +2,25 @@ $(document).ready(function(){
     const slider1 = $(".slider").bxSlider({
         controls:false
     });
-   
-    $(".toggle").click(function{
-        const toggle = $(".toggle").attr("class");
+    $(".toggle").click(function(){
+        const toggle = $(".toggle").attr("class"); 
         console.log(toggle);
 
         if( toggle == "toggle"){
+           
             document.getElementsByClassName("toggle")[0].classList.add("active");
-            $("gnb").removeClass("view");
+            $(".gnb").addClass("view");
         }else{
+            
             document.getElementsByClassName("toggle")[0].classList.remove("active");
             $(".gnb").removeClass("view");
         }
     });
 
-    $(window).resize(function(){
-        $(".toggle").removeClass("active");
-        $(".gnb").removeClass("view");
-    });
+    // $(window).resize(function(){
+    //     $(".toggle").removeClass("active");
+    //     $(".gnb").removeClass("view");
+    // });
+    
    
 });
