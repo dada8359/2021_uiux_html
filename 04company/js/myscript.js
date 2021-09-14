16c7cd0dec733985c6f0b90d3d33177ef966dac2
다다미무침 <87959460+dada8359@users.noreply.github.com>
2021-09-14 18:12:30 +0900

    up

--- 04company/js/myscript.js
+++ 04company/js/myscript.js
@@ -1,7 +1,24 @@
 $(document).ready(function(){
-   $(".slider").bxSlider({
+    const slider1 = $(".slider").bxSlider({
         controls:false
     });
    
+    $(".toggle").click(function{
+        const toggle = $(".toggle").attr("class");
+        console.log(toggle);
+
+        if( toggle == "toggle"){
+            document.getElementsByClassName("toggle")[0].classList.add("active");
+            $("gnb").removeClass("view");
+        }else{
+            document.getElementsByClassName("toggle")[0].classList.remove("active");
+            $(".gnb").removeClass("view");
+        }
+    });
+
+    $(window).resize(function(){
+        $(".toggle").removeClass("active");
+        $(".gnb").removeClass("view");
+    });
    
 });
